844606e98aefb098f11c364eb532abc2b24f3077
LongVH <longvh@vmodev.com>
2021-12-28 13:37:25 +0700

    add tutorial

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import styled from "styled-components";
 import { listParagraph } from "../common";
+import {
+  QuestionCircleOutlined,
+  SettingOutlined,
+  CloseCircleOutlined,
+} from "@ant-design/icons";
 
 const randomInterger = (maxNumber: number) => {
   return Math.floor(Math.random() * maxNumber);
@@ -20,6 +25,8 @@ export const Home = () => {
   const [isStart, setStart] = useState<boolean>(false);
   const [index, setIndex] = useState<number>(0);
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
+  const [isOpenTutorial, setOpenTutorial] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
 
   useLayoutEffect(() => {
     setState({ ...innitValue, index: randomInterger(listParagraph.length) });
@@ -64,6 +71,7 @@ export const Home = () => {
     }
     if (index === listParagraph[state.index].length - 1 || timeLeft === 0)
       setOpenModal(true);
+    setScore(wordPerMin * 2 - state.misTakes);
     // eslint-disable-next-line
   }, [timeLeft, index]);
 
@@ -90,9 +98,9 @@ export const Home = () => {
   });
 
   const messageTitle = () => {
-    if (wordPerMin < 40) {
+    if (score < 70) {
       return "u fuking bad bro(sis)!!!!";
-    } else if (wordPerMin > 100) {
+    } else if (wordPerMin > 170) {
       return "ur parent proud of u!!";
     } else {
       return "well done my friend!";
@@ -101,6 +109,18 @@ export const Home = () => {
 
   return (
     <Backgroud>
+      <Tutorial>
+        <div
+          className="question"
+          id="question"
+          onClick={() => setOpenTutorial(true)}
+        >
+          <QuestionCircleOutlined style={{ fontSize: 32 }} />
+        </div>
+        <div className="setting">
+          <SettingOutlined style={{ fontSize: 32 }} />
+        </div>
+      </Tutorial>
       <WrapperContainer>
         <div className="wrapper-content">
           <div className="content-info">{paragraphs}</div>
@@ -121,11 +141,38 @@ export const Home = () => {
             <div>Mistakes: {state.misTakes}</div>
             <div>Words per minute: {wordPerMin}</div>
             <div>Characters per minute: {characterPerMin}</div>
+            <div className="score">Ur score: {score}</div>
           </div>
           <div className="action">
             <button onClick={() => setOpenModal(false)}>OK</button>
           </div>
         </Modal>
+      )}
+      {isOpenTutorial && (
+        <TutorialModal>
+          <CloseCircleOutlined onClick={() => setOpenTutorial(false)} />
+          <div className="container">
+            <div className="title-tutorial">What is a typing test?</div>
+            <div className="content-tutorial">
+              A typing test is a practical test that measures your typing speed
+              and accuracy while working with an actual passage of text. While
+              tests vary, you're typically given a set piece of writing and a
+              set time to complete it within. Your test results look at four
+              scores, the number of character you can type per minute, known as
+              CPM or chracters-per-minute, the number of words you can type per
+              minute, known as WPM or words-per-minute, the number of errors you
+              have made in the text, and finally your score.
+            </div>
+            <div className="title-tutorial">How to calculate ur score?</div>
+            <div className="content-tutorial">
+              <ul>
+                <li>2 points for per word u finished.</li>
+                <li>-1 point for per mistake u made.</li>
+              </ul>
+            </div>
+            <div className="footer-tutorial">Good Luck!!!</div>
+          </div>
+        </TutorialModal>
       )}
     </Backgroud>
   );
@@ -134,6 +181,18 @@ export const Home = () => {
 const Backgroud = styled.div`
   height: 100vh;
   background-color: #d7dbc1;
+`;
+
+const Tutorial = styled.div`
+  display: flex;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  gap: 15px;
+  .question,
+  .setting {
+    cursor: pointer;
+  }
 `;
 
 const WrapperContainer = styled.div`
@@ -205,6 +264,10 @@ const Modal = styled.div`
   }
   .result {
     margin: 30px 0 0 105px;
+    .score {
+      color: #e14f4f;
+      font-weight: 500;
+    }
   }
   .action {
     text-align: center;
@@ -217,6 +280,46 @@ const Modal = styled.div`
       border-radius: 5px;
       background-color: #d7dbc1;
       cursor: pointer;
+    }
+  }
+`;
+
+const TutorialModal = styled.div`
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  z-index: 999;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid rgb(0 0 0 / 25%);
+  background-color: white;
+  border-radius: 10px;
+  .anticon-close-circle {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+    font-size: 22px;
+    cursor: pointer;
+  }
+  .container {
+    padding: 30px 40px 30px;
+    .title-tutorial,
+    .footer-tutorial {
+      text-align: center;
+      font-weight: 500;
+    }
+    .title-tutorial {
+      margin-top: 20px;
+    }
+    .content-tutorial {
+      margin-top: 20px;
+      ul {
+        margin-left: -20px;
+      }
+    }
+    .footer-tutorial {
+      margin-top: 40px;
     }
   }
 `;
